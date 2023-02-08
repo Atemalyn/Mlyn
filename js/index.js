b244894f4c45720674295c712e5c1c5d3a741208
Atemalyn <ciprianomarilyn27@gmail.com>
2023-02-08 17:15:29 +0800

    added files

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-var student =(
-    "name;" "Marilyn" ,
-     "surname;" "Cipriano",
-      "age;" "36")
-      ;
-document.write (student);
+
+function total(){
+
+  var item1;
+  var item2;item1=document.getElementById("n1").value * 12;
+  item2=document.getElementById("n2").value * 6;
+
+total_amount = item1 + item2;
+
+window.alert(total_amount);
+}
+
+
