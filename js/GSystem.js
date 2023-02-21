f9c1be1cfbad2526f90b40fb5c096f15f4150a0c
Atemalyn <ciprianomarilyn27@gmail.com>
2023-02-21 01:46:49 -0800

    add latest files modified

--- js/GSystem.js
+++ js/GSystem.js
@@ -10,14 +10,17 @@ var Grad3;
 
 
 Grad1 = document.getElementById("G1").value;
-
 Grad2 = document.getElementById("G2").value;
-
 Grad3 = document.getElementById("G3").value;
-Average= Grad1 + Grad2 + Grad3n/ 3 ;
 
+if (Grad1>100 || Grad2>100 || Grad3>|| 100);
+alert("Enter Average");
+}
+else{
 
-alert(Average);
+var Ave = Grad1 + Grad2 +Grad3;
+var Ave = Ave/3;
+document.getElementById("Average").innerHTML="Your Average is :" +Ave;
 
 
 }
